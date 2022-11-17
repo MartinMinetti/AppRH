cd3d90eac37c4d90ed3a1396f6da49ed36f32d5b
MartinMinetti <minettimartin0@gmail.com>
2022-11-17 19:22:16 -0300

    PagCasiFinish

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using AppRH.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppRHContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppRHContext") ?? throw new InvalidOperationException("Connection string 'AppRHContext' not found.")));
 
+builder.Services.AddDbContext<AppRHIdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppRHIdentityDbContext") ?? throw new InvalidOperationException("Connection string 'AppRHIdentityDbContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppRHIdentityDbContext>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<IdentityOptions>(options =>
+ {
+     // Password settings.
+     options.Password.RequireDigit = false;
+     options.Password.RequireLowercase = false;
+     options.Password.RequireNonAlphanumeric = true;
+     options.Password.RequireUppercase = false;
+     options.Password.RequiredLength = 6;
+     options.Password.RequiredUniqueChars = 0;
+});
 
 var app = builder.Build();
 
@@ -21,6 +40,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
@@ -28,4 +48,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
